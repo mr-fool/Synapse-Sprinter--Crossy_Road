4a7a6c2e162c7bbdf3a644c206105348705ad8a2
Aaronplayz14 <chiuaaron6a07@gmail.com>
2023-11-18 20:42:00 -0700

    add drawing

--- sprites.py
+++ sprites.py
@@ -1,7 +1,6 @@
 import pygame
 import random
 import requests
-from PIL import Image
 from io import BytesIO
 def load_image(url):
     response = requests.get(url)
@@ -13,16 +12,9 @@ repo_url = "https://github.com/KelvinCreighton/Arduino_Crossy_Road.git"
 
 
 # List of character URLs
-cars =["car.png, car_2.png, car_3.png"]
-playerpic=["player.png, player_2.png, player_3.png, player_4.png, player_5.png, player_1.png"]
-tree = ["tree.png, log.png"]
-picture_path = random.choice(playerpic)
-
-# Open the image using Pillow
-img = Image.open(picture_path)
-
-# Display the image
-img.show()
+cars =["car.png", "car_2.png", "car_3.png"]
+playerpic=["player.png", "player_2.png", "player_3.png", "player_4.png", "player_5.png", "player_1.png"]
+tree = ["tree.png", "log.png"]
 
 
 class Player:
@@ -30,6 +22,10 @@ class Player:
         self.x = x
         self.y = y
         self.rect = pygame.Rect(self.x, self.y, w, h)
+        playerrandom = random.choices(playerpic)
+        self.image = pygame.image.load(playerrandom[0])
+        self.image_ract = self.rect
+        self.resized_image = pygame.transform.scale(self.image, (w*3, h*3))
         self.color = (0, 128, 255)
         self.lives = 3
 
@@ -51,7 +47,7 @@ class Player:
         return self.rect.x+self.rect.w < 0 or self.rect.x > screen_width or self.rect.y+self.rect.h < 0 or self.rect.y > screen_height
     
     def draw(self, screen):
-        pygame.image.load(random.choice(playerpic))
+        screen.blit(self.resized_image, self.image_ract)
         
 
 
@@ -62,13 +58,22 @@ class Enemy:
         self.rect = pygame.Rect(self.x, self.y, w, h)
         self.speed = speed
         self.color = (255, 0, 0)
+        carsrandom = random.choices(cars)
+        self.image = pygame.image.load(carsrandom[0])
+        self.image_ract = self.rect
+        self.resized_image = pygame.transform.scale(self.image, (w*3, h*3))
+        if self.speed ==0:
+            treerandom = random.choices(tree)
+            self.image = pygame.image.load(treerandom[0])
+            self.image_ract = self.rect
+            self.resized_image = pygame.transform.scale(self.image, (w*3, h*3))
 
     def update(self, cam_x, cam_y):
         self.rect.x = self.x + cam_x
         self.rect.y = self.y + cam_y
     
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
+         screen.blit(self.resized_image, self.image_ract)
     
     def move(self):
         self.x += self.speed
